df4a087f525e4c9a84901bad16717ce7f5c7d7bc
Suraj Kulkarni <kulkarnis117@gmail.com>
2024-01-25 15:11:47 +0530

    added edit & find API's

--- routes/job.js
+++ routes/job.js
@@ -30,4 +30,83 @@ router.post("/create",jwtVerify, async (req,res)=>{
     }
 });
 
+
+router.put("/edit/:jobId",jwtVerify, async (req,res)=>{
+    try {
+        const { companyName, title, description, logoUrl} = req.body;
+        const jobId = req.params.jobId;
+
+        if (!companyName || !title || !description || !logoUrl || !jobId) {
+            return res.status(400).json({
+                errorMessage: "Bad Request"
+            });
+        }
+
+        await Job.updateOne(
+          { _id: jobId },
+          {
+            $set: {
+              companyName,
+              title,
+              description,
+              logoUrl,
+            },
+          }
+        );
+
+        res.json({message: "Job details updated successfully"})
+
+    }catch(error){
+        console.log(error);
+    }
+});
+
+
+router.get("/job-description/:jobId", async (req,res)=>{
+    try {
+        const jobId = req.params.jobId;
+
+        if (!jobId) {
+            return res.status(400).json({
+                errorMessage: "Bad Request"
+            });
+        }
+
+        const jobDetails = await Job.findById(jobId);
+
+        res.json({data: jobDetails});
+
+    }catch(error){
+        console.log(error);
+    }
+});
+
+
+router.get("/all", async (req,res)=>{
+    try {
+        const jobList = await Job.find({}, { companyName: 1, title: 1});
+
+        res.json({data: jobList});
+
+    }catch(error){
+        console.log(error);
+    }
+});
+
+
+router.get("/search", async (req,res)=>{
+    try {
+        const title = req.query.title || "";
+        const searchResult = await Job.find(
+            { title : {$regex: title, $options: "i"}},
+            { companyName: 1, title: 1});
+
+        res.json({data: searchResult});
+
+    }catch(error){
+        console.log(error);
+    }
+});
+
+
 module.exports = router;
