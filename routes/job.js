97fe19675de998216a0b01bb8faa9f0907d3579e
Suraj Kulkarni <kulkarnis117@gmail.com>
2024-01-26 17:53:22 +0530

    added feature in find=search API

--- routes/job.js
+++ routes/job.js
@@ -97,9 +97,20 @@ router.get("/all", async (req,res)=>{
 router.get("/search", async (req,res)=>{
     try {
         const title = req.query.title || "";
+        const skills = req.query.skills;
+        let filterSkills = skills?.split(",");  // skills ? skills.split(",") : null;  instead of this this is used ==> skills?.split(",")
+
+        let filter = {};
+
+        if (filterSkills) {
+             filter = { skills : {$in: [...filterSkills]}};
+        }
+
         const searchResult = await Job.find(
-            { title : {$regex: title, $options: "i"}},
-            { companyName: 1, title: 1});
+            { title : {$regex: title, $options: "i"},
+              ...filter,
+            },
+            { companyName: 1, title: 1, skills: 1});
 
         res.json({data: searchResult});
 
